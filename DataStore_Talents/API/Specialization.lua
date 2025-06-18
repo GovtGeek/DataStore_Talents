cd583a30f2d22adbda10f049a23c8f813fd95c9a
GovtGeek <26489473+GovtGeek@users.noreply.github.com>
2025-06-18 00:24:39 -0700

    Update Specialization.lua
    Fixed calls to use proper namespace
    Mists uses GetSpecInfo_Retail
    Listen to PLAYER_ENTERING_WORLD for ScanSpecialization

--- DataStore_Talents/API/Specialization.lua
+++ DataStore_Talents/API/Specialization.lua
@@ -7,14 +7,16 @@ local GetSpecialization, GetSpecializationInfo = GetSpecialization, GetSpecializ
 
 local bit64 = LibStub("LibBit64")
 local isRetail = (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE)
+local isCataclysm = (LE_EXPANSION_LEVEL_CURRENT == LE_EXPANSION_CATACLYSM)
+local isMists = LE_EXPANSION_LEVEL_CURRENT == LE_EXPANSION_MISTS_OF_PANDARIA
 
 -- *** Scanning functions ***
 local function GetSpecInfo_Retail()
-	local specID = GetSpecialization()
-	local _, specName, _, _, role = GetSpecializationInfo(specID)
-	
-	roleID = DataStore:StoreToSetAndList(specInfos.Roles, role)
-	
+	local specID = C_SpecializationInfo.GetSpecialization()
+	local _, specName, _, _, role = C_SpecializationInfo.GetSpecializationInfo(specID)
+
+	local roleID = DataStore:StoreToSetAndList(specInfos.Roles, role)
+
 	return specID, specName, roleID
 end
 
@@ -42,12 +44,12 @@ local function ScanSpecialization()
 	
 	local specID, specName, roleID
 	
-	if isRetail then
+	if isRetail or isMists then
 		specID, specName, roleID = GetSpecInfo_Retail()
-	else
+	elseif isCataclysm then
 		specID, specName, roleID = GetSpecInfo_Cataclysm()
 	end
-	
+
 	local nameID = DataStore:StoreToSetAndList(specInfos.Names, specName)
 	
 	specializations[DataStore.ThisCharID] = specID 	-- bits 0-2 : active spec index
@@ -99,8 +101,9 @@ DataStore:OnAddonLoaded(addonName, function()
 end)
 
 DataStore:OnPlayerLogin(function()
+	addon:ListenTo("PLAYER_ENTERING_WORLD", ScanSpecialization)
 	addon:ListenTo("PLAYER_ALIVE", ScanSpecialization)
-	if isRetail then
+	if isRetail or isMists then
 		addon:ListenTo("PLAYER_SPECIALIZATION_CHANGED", ScanSpecialization)
 	else
 		addon:ListenTo("CHARACTER_POINTS_CHANGED", ScanSpecialization)
